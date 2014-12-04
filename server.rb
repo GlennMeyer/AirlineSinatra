51dd8ef803358224b44a8db90b6f77563a373fc6
Glenn Meyer <glennmeyer@gmail.com>
2014-12-03 18:44:30 -0600

    data tweaks

--- server.rb
+++ server.rb
@@ -7,16 +7,19 @@ set :bind, '0.0.0.0'
 get '/' do
   db = PG.connect(host: 'localhost', dbname: 'on_time_performance')
 
+  # How many different airlines are represented in this dataset?
+
   qone = <<-SQL
     SELECT COUNT(DISTINCT carrier)
     FROM on_time_performance;
   SQL
 
   @carriers = db.exec(qone)
-  # @carriers.each {|x| puts x}
+
+  # Which airline had the largest quantity of delayed arriving flights? Which had the fewest?
 
   qtwo = <<-SQL
-    SELECT count(DEP_DELAY_NEW) + count(ARR_DELAY_NEW) AS count, CARRIER
+    SELECT count(ARR_DELAY_NEW) AS count, CARRIER
     FROM on_time_performance
     WHERE DEP_DELAY_NEW > 0
     OR ARR_DELAY_NEW > 0
@@ -24,8 +27,9 @@ get '/' do
     ORDER BY count DESC;
   SQL
 
-  @mostleast = db.exec(qtwo)
-  # @mostleast.each {|x| puts x}
+  @carrierdelays = db.exec(qtwo)
+
+  # Which departing airport had the highest number of delayed flights? Which had the fewest?
 
   qthree = <<-SQL
     SELECT count(DEP_DELAY_NEW), ORIGIN_CITY_NAME
@@ -37,6 +41,8 @@ get '/' do
 
   @departeddelays = db.exec(qthree)
 
+  # Which arriving airport had the highest number of delayed flights? Which had the fewest?
+
   qfour = <<-SQL
     SELECT count(ARR_DELAY_NEW), DEST_CITY_NAME
     FROM on_time_performance
@@ -46,6 +52,8 @@ get '/' do
 
   @arrivingdelays = db.exec(qfour)
 
+  # What was the average number of minutes late across all airlines?
+
   qfive = <<-SQL
     SELECT ( (AVG(ARR_DELAY_NEW) + AVG(DEP_DELAY_NEW) ) / 2 ) AS average_minutes_late
     FROM on_time_performance;
@@ -53,8 +61,10 @@ get '/' do
 
   @averagemin = db.exec(qfive)
 
+  # What was the average number of minutes late for each airline?
+  
   qsix = <<-SQL
-    SELECT CARRIER, ( SUM(ARR_DELAY_NEW) + SUM(DEP_DELAY_NEW) ) / ( COUNT(ARR_DELAY_NEW) + COUNT(DEP_DELAY_NEW) ) AS average_minutes_late
+    SELECT CARRIER, ( AVG(ARR_DELAY_NEW) + AVG(DEP_DELAY_NEW) ) / 2 AS average_minutes_late
     FROM on_time_performance
     GROUP BY CARRIER
     ORDER BY average_minutes_late desc;
